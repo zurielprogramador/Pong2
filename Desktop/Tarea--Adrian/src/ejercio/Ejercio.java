28c6d38567cf5e24d44eed7e904251104e18a8b6
zurielprogramador <argenis.canton08@gmail.com>
2022-09-22 01:38:59 -0500

    hola

---
+++ Desktop/Tarea--Adrian/src/ejercio/Ejercio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercio;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author kevin
+ */
+public class Ejercio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"SOY KEVIN CHIQUIL");
+        
+    }
+    
+}
