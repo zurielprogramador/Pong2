aa47b311eef96be776ab55a5ad9b35489f43aab5
zurielprogramador <argenis.canton08@gmail.com>
2022-09-22 02:00:01 -0500

    zuriel

---
+++ Desktop/Tarea-ya-treminada-master/src/proyectodefinitivo/PROYECTODEFINITIVO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectodefinitivo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author kevin
+ */
+public class PROYECTODEFINITIVO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "SOY KEVIN ROBERTO CHIQUIL CHIN");
+        JOptionPane.showMessageDialog(null, "SOY ANGEL ADRIAN KAU MIS");
+         JOptionPane.showMessageDialog(null, "SOY ZURIEL ARGENIS CANTON DZIB");
+    }
+    
+}
